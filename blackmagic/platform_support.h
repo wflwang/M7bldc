21c9c92528690e6874fbaac6c681335d79ba3d2e
wfl <wflwflwang@126.com>
2025-03-05 00:12:52 +0800

    简化编译版本

--- blackmagic/platform_support.h
+++ blackmagic/platform_support.h
@@ -1,20 +1,11 @@
 /*
- * This file is part of the Black Magic Debug project.
- *
- * Copyright (C) 2015 Gareth McMullin <gareth@blacksphere.co.nz>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Date: 2024/10/20
+    Author: MaxwellWang
+    Describe: 
+    Program:
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __PLATFORM_SUPPORT_H
---
+++ hwconf/luna/m600/hw_luna_m600_Rev5_60V.h
@@ -0,0 +1,19 @@
+/*
+    Date: 2024/10/20
+    Author: MaxwellWang
+    Describe: 
+    Program:
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#ifndef HW_LUNA_M600_REV5_60V_H_
+#define HW_LUNA_M600_REV5_60V_H_
+
+#define M600_Rev5
+#define M600_60V_BATTERY
+
+#include "hw_luna_m600.h"
+
+#endif /* HW_LUNA_M600_REV5_60V_H_ */
