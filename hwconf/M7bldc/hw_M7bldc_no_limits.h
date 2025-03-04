b6348defe015a28437a9c6ca9a26ac06c0e54636
wfl <wflwflwang@126.com>
2025-03-04 23:20:44 +0800

    M7BLDC start

---
+++ hwconf/M7bldc/hw_M7bldc_no_limits.h
@@ -0,0 +1,15 @@
+/*
+    Date: 2024/10/20
+    Author: MaxwellWang
+    Describe: 
+    Program:
+    */
+
+#ifndef HW_M7bldc_NO_LIMITS_H_
+#define HW_M7bldc_NO_LIMITS_H_
+
+#define DISABLE_HW_LIMITS
+
+#include "hw_M7bldc.h"
+
+#endif /* HW_M7bldc_NO_LIMITS_H_ */
